4599006ce937e903d8853b4b44a2422cd8317e1c
zhenfong07 <phongchanngo@gmail.com>
2025-12-24 10:27:09 +0800

    debug FreeRTOS with main.asmbin test

--- software/main.c
+++ software/main.c
@@ -1,58 +1,27 @@
-/*
- * main.c - VGA PRIORITY TEST
- * Objective: Skip UART checks to force VGA to run
- */
+#include "FreeRTOS.h"
+#include "task.h"
 
-#include <stdint.h>
+volatile int working = 0;
 
-#define UART_BASE       0x40000000
-#define UART_TX_DATA    ( *(volatile uint32_t *)(UART_BASE + 0x10) )
-
-#define VGA_BASE        0x20000000
-#define VGA_CTRL        ( *(volatile uint32_t *)(VGA_BASE + 0x04) )
-#define VGA_PALETTE(i)  ( *(volatile uint32_t *)(VGA_BASE + 0x20 + ((i)<<2)) )
-
-/* "Blind" UART Driver - Does not check status */
-/* Helps avoid hanging if hardware is not ready */
-void uart_putc(char c) {
-    UART_TX_DATA = c; 
-    // Add a small delay to let the computer process
-    for(int i=0; i<1000; i++) __asm__("nop");
-}
-
-void uart_puts(const char *s) {
-    while (*s) uart_putc(*s++);
-}
-
-void busy_wait(int count) {
-    volatile int i;
-    for (i = 0; i < count; i++) __asm__("nop");
+void test_task(void *arg) {
+    while (1) {
+        working++;
+        for (volatile int i = 0; i < 100000; i++);
+    }
 }
 
 int main(void) {
-    /* 1. TEST VGA IMMEDIATELY (First instruction) */
-    VGA_CTRL = 1; // Turn on screen
+    xTaskCreate(
+        test_task,
+        "test",
+        256,
+        NULL,
+        1,
+        NULL
+    );
+
+    vTaskStartScheduler();
+
     
-    // Write RED color to Palette 0 (Background)
-    VGA_PALETTE(0) = 0xE0; 
-
-    /* 2. Send via UART (If text appears -> UART works. If not -> ignore) */
-    uart_puts("VGA Red Test\r\n");
-
-    /* 3. Loop to change colors continuously */
-    while (1) {
-        // GREEN
-        VGA_PALETTE(0) = 0x1C; 
-        busy_wait(500000); // Delay ~0.5s
-
-        // WHITE
-        VGA_PALETTE(0) = 0xFF; 
-        busy_wait(500000);
-        
-        // RED
-        VGA_PALETTE(0) = 0xE0;
-        busy_wait(500000);
-    }
-
-    return 0;
+    while (1);
 }
